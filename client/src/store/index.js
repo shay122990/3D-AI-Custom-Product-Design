659663839313a13a5772b03ccbbf42e11ce557fd
Shay <moeshakasanova@gmail.com>
2024-09-03 17:51:17 +0400

    change to shay-development logo

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -5,8 +5,8 @@ const state = proxy({
   color: "#EFBD48",
   isLogoTexture: true,
   isFullTexture: false,
-  logoDecal: "./threejs.png",
-  fullDecal: "./threejs.png",
+  logoDecal: "./sd-logo.png",
+  fullDecal: "./sd-logo.png",
 });
 
 export default state;
