178e265e0faea5de904a3abcaf1079ed446a4492
Shay <moeshakasanova@gmail.com>
2024-09-03 18:58:31 +0400

    setting logo and logo full options

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -36,13 +36,30 @@ const Customizer = () => {
       case "colorpicker":
         return <ColorPicker />;
       case "filepicker":
-        return <FilePicker />;
+        return (
+          <FilePicker file={file} setFile={setFile} readFile={readFile()} />
+        );
       case "aipicker":
         return <AIPicker />;
 
       default:
         return null;
     }
+  };
+  const handleDecals = (type, result) => {
+    const decalType = DecalTypes[type];
+
+    state[decalType.stateProperty] = result;
+
+    if (!activeFilterTab[decalType.filterTab]) {
+      handleActiveFilterTab(decalType.filterTab);
+    }
+  };
+  const readFile = (type) => {
+    reader(file).then((result) => {
+      handleDecals(type, result);
+      setActiveEditorTab("");
+    });
   };
 
   const handleActiveFilterTab = (tabName) => {
