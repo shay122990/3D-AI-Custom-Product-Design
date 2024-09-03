9036d695021691a2744740be038f941e6142f085
Shay <moeshakasanova@gmail.com>
2024-09-03 16:35:58 +0400

    tab layout

--- client/src/components/Tab.jsx
+++ client/src/components/Tab.jsx
@@ -1,5 +1,29 @@
-const Tab = () => {
-  return <div>Tab</div>;
+import { useSnapshot } from "valtio";
+import state from "../store";
+
+const Tab = ({ tab, IsFilterTab, isActiveTab, handleClick }) => {
+  const snap = useSnapshot(state);
+  const activeStyles =
+    IsFilterTab && isActiveTab
+      ? { backgroundColor: snap.color, opacity: 0.5 }
+      : { backgroundColor: "transparent", opacity: 1 };
+  return (
+    <div
+      key={tab.name}
+      className={`tab-btn ${
+        IsFilterTab ? "rounded-full glassmorhism" : "rounded-4"
+      }`}
+      onClick={handleClick}
+    >
+      <img
+        src={tab.icon}
+        alt={tab.name}
+        className={`${
+          IsFilterTab ? "w-2/3 h-2/3" : "w-11/12 h-11/12 object-contain"
+        }`}
+      />
+    </div>
+  );
 };
 
 export default Tab;
